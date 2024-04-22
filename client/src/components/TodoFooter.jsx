67f463707e776e9409d4dae132288f6d877e2b0c
seonyeonghun2 <kjca1117>
2024-04-22 18:27:54 +0900

    푸터 조건부렌더링 적용

--- client/src/components/TodoFooter.jsx
+++ client/src/components/TodoFooter.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const TodoFooter = ({count}) => {
+const TodoFooter = ({ count }) => {
   return (
-    <div className='w-full pt-2 mt-5 text-center border-t-2'>총 {count}건의 할일이 등록되어 있습니다.</div>
-  )
-}
+    <div className='w-full pt-2 text-center border-t-2'>
+      {count <= 0 ? <span>할일을 등록하세요</span>:<span>총 {count}건의 할일이 등록되어 있습니다.</span>}
+    </div>
+  );
+};
 
-export default TodoFooter
+export default TodoFooter;
