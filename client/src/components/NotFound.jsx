fcd30e3d5600fd4631782a3289db6232c945d109
seonyeonghun2 <kjca1117@naver.com>
2024-05-07 18:18:33 +0900

    404 컴포넌트 추가

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>잘못된 경로로 접근했습니다.</h1>
+      <p>404 | Page Not Found</p>
+    </div>
+  );
+};
+
+export default NotFound;
